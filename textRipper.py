b0210b83abc9e05e1b4c43b2d4e6759dfd74d05c
blinno <business.kozlowskihubert@gmail.com>
2023-10-05 21:28:20 +0100

    fixed.

--- textRipper.py
+++ textRipper.py
@@ -9,16 +9,15 @@ def draw_red_dot(x, y):
     pyautogui.moveTo(x, y, duration=0.2)
     pyautogui.click(button='right')  # Right-click to draw a red dot
 
-# Display a message to prompt the user to select the region
+# Prompt the user to select the top-left corner
 print("Please control-click the top-left corner of the region.")
-top_left = pyautogui.confirm("Control-click the top-left corner of the region.")
+top_left_x, top_left_y = pyautogui.position()
+draw_red_dot(top_left_x, top_left_y)
 
+# Prompt the user to select the bottom-right corner
 print("Please control-click the bottom-right corner of the region.")
-bottom_right = pyautogui.confirm("Control-click the bottom-right corner of the region.")
-
-# Parse the coordinates of the selected region
-top_left_x, top_left_y = map(int, top_left.split(','))
-bottom_right_x, bottom_right_y = map(int, bottom_right.split(','))
+bottom_right_x, bottom_right_y = pyautogui.position()
+draw_red_dot(bottom_right_x, bottom_right_y)
 
 # Calculate the width and height of the selected region
 width = bottom_right_x - top_left_x
@@ -26,10 +25,6 @@ height = bottom_right_y - top_left_y
 
 # Take a screenshot of the selected area
 screenshot = pyautogui.screenshot(region=(top_left_x, top_left_y, width, height))
-
-# Draw red dots at the selected corners
-draw_red_dot(top_left_x, top_left_y)
-draw_red_dot(bottom_right_x, bottom_right_y)
 
 # Convert the screenshot to a format that can be drawn on
 screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
